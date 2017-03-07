27c30dd1294f8f43cdb5f7bb842627cf4625bbae
Maycon D <maycout2@gmail.com>
2017-03-07 12:38:47 -0300

    Acrescentando o monstro

--- Assets/Scripts/MoveEsquerda.cs
+++ Assets/Scripts/MoveEsquerda.cs
@@ -13,7 +13,7 @@ public class MoveEsquerda : MonoBehaviour {
     void Start () {
 		
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 
---
+++ Assets/Scripts/MoveMorcego.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveMorcego : MonoBehaviour
+{
+
+    public float velocidade = 1f;
+    public float velocidadeHorizontal;
+    public float velocidadeVertical;
+    public float min;
+    public float max;
+    public float espera;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(Move(max));
+    }
+
+   IEnumerator Move(float destino)
+    {
+        while(Mathf.Abs(destino - transform.localPosition.y) > 0.3f) {
+
+   
+            Vector3 direcao2 = (destino == max) ? Vector3.up : Vector3.down;
+
+           
+            Vector3 velocidadeVetorial2 = direcao2 * velocidadeVertical;
+
+            transform.localPosition = transform.localPosition + velocidadeVetorial2 * Time.deltaTime;
+
+            yield return null;
+
+
+         
+
+
+        }
+
+        yield return new WaitForSeconds(espera);
+
+        destino = (destino == max) ? min : max;
+        StartCoroutine(Move(destino));
+    }
+
+    void Update()
+    {
+
+        Vector3 velocidadeVetorial = Vector3.left * velocidade;
+
+        transform.position = transform.position + velocidadeVetorial * Time.deltaTime;
+
+
+    }
+    }
